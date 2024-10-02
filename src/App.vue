7d8cb9a7bd85841c3927b5caaf324eede9e1f79a
David LaJambe <dlajambe@gmail.com>
2024-10-02 18:38:17 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Block from './components/Block.vue';
+import { ref } from 'vue'
+
+let isPlaying:boolean = false;
+const delay = ref();
+const timeTaken = ref();
+// let startTime = performance.now();
+
+function startGame() {
+  isPlaying = true;
+  delay.value = Math.random()*(7 - 2) + 2;
+  console.log(delay.value);
+  startTime = performance.now()
+}
+
+function stopTimer() {
+  timeTaken.value = performance.now() - startTime;
+  isPlaying = false;
+  console.log(timeTaken.value)
+}
+
+</script>
+
+<template>
+  <h1>
+    Ninja Reaction Timer
+  </h1>
+  <p>{{ delay }}</p>
+  <button @click="startGame">Play</button>
+  <Block @click="stopTimer"/>
+  <
+</template>
+<style>
+
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="block">
+    Click me!
+  </div>
+</template>
+
+<style>
+
+.block {
+    width: 400px;
+    height: 200px;
+    border-radius: 20px;
+    background-color: aqua;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+</style>
